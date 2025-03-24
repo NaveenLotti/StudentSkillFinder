661970df634ec04cab84f23f2c2a701e9cfc9bc0
manoj-kumar14 <manojkumar.ankusarao@gmail.com>
2025-03-24 20:04:46 +0530

    type-1
    font page

--- src/components/StudentSearch.jsx
+++ src/components/StudentSearch.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useState } from 'react'
-import "./StudentSearch.css"
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import "./StudentSearch.css";
+import { useNavigate } from "react-router-dom";
+import logo from "../assest/pic.jpeg";
 
 const StudentSearch = () => {
     const [rollNo, setRollNo] = useState("");
@@ -9,27 +9,40 @@ const StudentSearch = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!rollNo){
+        if (!rollNo) {
             alert("Please enter a roll number");
-            return
+            return;
         }
         navigate(`/student/${rollNo}`);
-    }
+    };
 
     return (
-        <div className="search-div">
-            <div className="student-search">
-            <label htmlFor="rollNo">Roll Number:</label>
-            <input type="text" 
-            name="rollNo"
-            placeholder="Enter Roll Number" 
-            value={rollNo} 
-            onChange={(e)=>setRollNo(e.target.value)}/>
-        </div>
-        <button onClick={handleSubmit}>Search</button>
-        </div>
-        
-    )
-}
+        <div>
+            <div className="navbar">
+                <img src={logo} alt="logo" />
+                <div className="nav-left">
+                </div>
+                <div className="nav-right">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Help</a>
+                </div>
+            </div>
+            <div className="card" style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+                <div className="student-search">
+                    <input
+                        type="text"
+                        name="rollNo"
+                        placeholder="Enter Roll Number"
+                        value={rollNo}
+                        onChange={(e) => setRollNo(e.target.value)}
+                    />
+                    <button onClick={handleSubmit}>Search</button>
+                </div>
+            </div>
+        </div >
+    );
+};
 
-export default StudentSearch
+export default StudentSearch;
