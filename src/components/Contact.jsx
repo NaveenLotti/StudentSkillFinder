7cb6a80b97b879d46e29e4f2928bb713821eb978
Lokesh-mulleti <mulletilokesh.m@gmail.com>
2025-03-26 23:08:57 +0530

    footer-1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,17 @@ import { FaTwitter, FaFacebookF, FaInstagram, FaWhatsapp, FaLinkedinIn } from "r
 const Contact = () => {
   return (
     <footer id="footer">
+      <div className="footer-content">
+          <div className="logo">
+             <img src="src/assests/logo.png" alt="logo" />
+          </div>
+        <div className="links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Help</a>
+          
+        </div>
       <div className="social-links">
         <a href="#" className="twitter"><FaTwitter /></a>
         <a href="#" className="facebook"><FaFacebookF /></a>
@@ -12,6 +23,8 @@ const Contact = () => {
         <a href="#" className="linkedin"><FaLinkedinIn /></a>
         <a href="#" className="whatsapp"><FaWhatsapp /></a>
       </div>
+      </div>
+
     </footer>
   );
 };
